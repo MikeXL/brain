305d082dfffa730daa2a5904450709d02cb4b1e3
Mike <Mike@mscanada.net>
2017-11-11 00:08:00 -0800

    hand coded version with couple parameters

--- R/neuron.r
+++ R/neuron.r
@@ -1,32 +1,21 @@
 
-#
-# now only handles single parameter x
-#
 
-
-perceptron <- function (w, x, b=1.0) {
-  activation((w*x + b))
-}
-
-activation <- function (s, t=0){
-  s>=t    # t is threshold
-}
-
-cost <- function() {
-  .0
-}
-
-update <- function(w, x, alpha=1.0, y, y.hat) {
-  w + alpha*(y-y.hat)*x
-}
-
-
-train <- function (x, y, iter=100, learning.rate=1.0, bias=1.0){
-  w <- rnorm(length(x))
-  
-  for(i in 1:iter) {
-    y.hat <- perceptron(w, x, bias)
-    w <- update(w, x, learning.rate, y, y.hat)
-    print(sum(y!=y.hat))
+n.iter = 10000                                                # number of iterations
+n.obs = nrow(x)                                               # number of samples / observations
+n.parm = ncol(x)                                              # number of parameters / input / features
+b <- 0                                                        # bias
+alpha <- 1                                                    # learning rate
+y.hat <- rep(0, n.obs)                                        # predications y.hat, baseline
+w <- rnorm(2)                                                 # initialize weight
+for(i in 1:n.iter) {
+  for(j in 1:n.obs){
+    y.hat[j] = (tanh(w[1]*x[j, 1] + w[2]*x[j, 2] + b) >=0)    # tanh activation
+    eta <- y[j] - y.hat[j]                                    # error
+    w[1] = w[1] + alpha * eta * x[j,1]                        # update w1
+    w[2] = w[2] + alpha * eta * x[j,2]                        # update w2
+    
   }
 }
+
+w                                                             # final weights
+sum(y.hat != y)                                               # misclassification rate
