072533ac17d6ef41593ef93b364f542ea8f435de
Mike <MikeXL@users.noreply.github.com>
2020-07-30 14:08:23 -0700

    tidy the PSO

--- nn.f
+++ nn.f
@@ -4,22 +4,22 @@ C  as well trying out new mechanism
 C    v[] = v[] + c1 * rand() * (pbest[] - present[]) + c2 * rand() * (gbest[] - present[])    (a)
 C    present[] = persent[] + v[]                                                              (b)
 C 
-C For each particle
+C FOR each particle
 C    Initialize particle
 C END
 C
-CDo
-C    For each particle
+C DO
+C    FOR each particle
 C        Calculate fitness value
 C        If the fitness value is better than the best fitness value (pBest) in history
 C            set current value as the new pBest
-C    End
+C    END
 C
 C    Choose the particle with the best fitness value of all the particles as the gBest
-C    For each particle
+C    FOR each particle
 C        Calculate particle velocity according equation (a)
 C        Update particle position according equation (b)
-C    End
+C    END
 C While maximum iterations or minimum error criteria is not attained
 C23456789
       
@@ -28,24 +28,21 @@ C
 C  two hidden layers network for now, code can be changed to accomondate more complex
 C  network topology a.ka. architecture, structure
 C   input (n*m) -> h1 (12+1) -> h2 (7+1) -> output (n*k)
-        parameter (nh1=12, nh2=7, iter=100, nparticle=1000, c1=2, c2=2)
-        dim(nparticle, m,     nh+1)    :: p1
-        dim(nparticle, nh+1,  nh+1)    :: p2
-        dim(nparticle, nh2+1, k)       :: p3
+        parameter (nh1=12, nh2=7, iter=100, c1=2, c2=2)
+        nparticle = m*(nh1+1) + (nh1+1)*(nh2+1) + (nh2+1)*k
+        double precision particle(nparticle)
         dim(m,      nh1+1)    :: w1, pbest1, gbest1
         dim(nh1+1,  nh2+1)    :: w2, pbest2, gbest2
         dim(nh2+1, k)         :: w3, pbest3, gbest3
         dim(n, k)             :: output, yhat
 C initialize weights
-        do 99 j=1, iter 
-        call random_number(p1)
-        call random_number(p2)
-        call random_number(p3)
+        do 99 i=1, iter 
+        call random_number(particle)
 C pbest
-        do 69 i=1, nparticle
-            w1 = reshape(p1(i, :, :), (/m, nh1+1/))
-            w2 = reshape(p2(i, :, :), (/nh+1, nh2+1/))
-            w3 = reshape(p3(i, :, :), (/nh2+1, k/))
+        do 69 j=1, nparticle
+            w1 = reshape(particle(1:m*(nh1+1)), (/m, nh1+1/))
+            w2 = reshape(particle((1+m*(nh1+1)):(m*(nh1+1)+(nh1+1)*(nh2+1))), (/nh+1, nh2+1/))
+            w3 = reshape(particle(:), (/nh2+1, k/))
             call fit(x, w1, w2, w3, yhat)
             mse = sum((y-yhat)**2) / size(y)
             if mse < pbest then 
@@ -63,21 +60,18 @@ C gbest
           gbest3 = pbest3
         end if 
 C update weights 
-        do 71 i=1, nparticle
+        do 71 j=1, nparticle
           w1 = w1 + c1*rand()*(pbest1-w1) + c2*rand()*(gbest1-w1)
           w2 = w1 + c1*rand()*(pbest2-w2) + c2*rand()*(gbest2-w2)
           w3 = w1 + c1*rand()*(pbest3-w3) + c2*rand()*(gbest3-w3)
   71    continue
   99    continue 
 C show me the trained network (weights)
-C        print *, w1, w2, w3
+C        print *, gbest1, gbest2, gbest3
 C prediction ? 
-C  call fit(newdata, gbest1, gbest2, gbest3, output) 
+C        call fit(newdata, gbest1, gbest2, gbest3, output) 
        end subroutine nn
+C fit the network
        subroutine fit(x, w1, w2, w3, yhat)
-         yhat = matmul(w3, 
-      +             matmul(w2, 
-      +                 matmul(w1, x)
-      +                   )
-      +             )
+         yhat = x*w1*w2*w3
        end subroutine fit
