69806201c526b634bda0728e19b8a2cf5e0c0b10
Mike <MikeXL@users.noreply.github.com>
2020-07-29 22:22:59 -0700

    yeee, first sketch PSO

--- nn.f
+++ nn.f
@@ -28,26 +28,52 @@ C
 C  two hidden layers network for now, code can be changed to accomondate more complex
 C  network topology a.ka. architecture, structure
 C   input (n*m) -> h1 (12+1) -> h2 (7+1) -> output (n*k)
-        parameter (nh1=12, nh2=7, iter=100, c1=2, c2=2)
-        dim(m,     nh+1)    :: w1, pbest1, gbest1, v1, p1
-        dim(nh+1,  nh+1)    :: w2, pbest2, gbest2, v2, p2
-        dim(nh2+1, k)       :: w3, pbest3, gbest3, v3, p3
+        parameter (nh1=12, nh2=7, nparticle=1000, c1=2, c2=2)
+        dim(nparticle, m,     nh+1)    :: p1
+        dim(nparticle, nh+1,  nh+1)    :: p2
+        dim(nparticle, nh2+1, k)       :: p3
+        dim(m,     nh1+1)    :: w1, pbest1, gbest1
+        dim(nh1+1,  nh2+1)    :: w2, pbest2, gbest2
+        dim(nh2+1, k)       :: w3, pbest3, gbest3
 C initialize weights
-        call random_number(w1)
-        call random_number(w2)
-        call random_number(w3)
-C training loop a.k.a epoch, iteration
-        do 69 i=1, iter
-            
-            do 60 j=1, size(w1)
-              call fit(x, w1, w2, w3, mse)
-              if mse < pbest then pbest = mse
- 60         continue
- 
-
+        call random_number(p1)
+        call random_number(p2)
+        call random_number(p3)
+C pbest
+        do 69 i=1, nparticle
+            w1 = reshape(p1, /m, nh1+1/)
+            w2 = rehspae(p2, /nh+1, nh2+1/)
+            w3 = reshape(p3, /nh2+1, k)
+            call fit(x, w1, w2, w3, y, mse)
+            if mse < pbest then 
+              pbest = mse
+              pbest1 = w1
+              pbest2 = w2
+              pbest3 = w3
+            end if
  69     continue
+C gbest
+        if pbest < gbest then 
+          gbest = pbest
+          gbest1 = pbest1
+          gbest2 = pbest2
+          gbest3 = pbest3
+        end if 
+C update weight 
+        do 71 i=1, nparticle
+          w1 = w1 + c1*rand()*(pbest1-w1) + c2*rand()*(gbest1-w1)
+          w2 = w1 + c1*rand()*(pbest2-w2) + c2*rand()*(gbest2-w2)
+          w3 = w1 + c1*rand()*(pbest3-w3) + c2*rand()*(gbest3-w3)
+  71    continue
 C show me the trained network (weights)
         print *, w1, w2, w3
 C prediction ? 
-C  output = f(f(f(newdata * w1) * w2) * w3)
+C  call fit(newdata, gbest1, gbest2, gbest3, output) 
        end subroutine nn
+       subroutine fit(x, w1, w2, w3, yhat)
+         yhat = matmul(w3, 
+      +             matmul(w2, 
+      +                 matmul(w1, x)
+      +                   )
+      +             )
+       end subroutine fit
