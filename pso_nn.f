45ab4bdb7ac886bff9bcae645b8f56f9bef924a6
M.J <5906118+mikexl@users.noreply.github.com>
2021-03-10 02:02:54 -0800

    Update pso_nn.f

--- pso_nn.f
+++ pso_nn.f
@@ -26,7 +26,7 @@ C        Calculate particle velocity according equation (a)
 C        Update particle position according equation (b)
 C    END
 C While maximum iterations or minimum error criteria is not attained
-        program pso
+        PRORAM PSO
         parameter (iter=10, w=2.6, c1=.8, c2=.8, npar=12, nd=2)
         double precision par(npar, nd), pbest(npar, nd)
         double precision r1(nd), r2(nd), gbest(nd)
@@ -61,31 +61,25 @@ C update particles
 99      continue       
          print *, gbest
          print *, fx(gbest)
-         end C 
+         END PROGRAM  
 C compute nn
 C more work to be done here 
-C      subroutine nn(x, y, n, m, k, newdata, output)
-       subroutine nn(w)
+       FUNCTION NN(w)
 C reshape w into w1,2, ...
 C
 C  two hidden layers network for now, code can be changed to accomondate more complex
 C  network topology a.ka. architecture, structure
-C   input (n*m) -> h1 (12+1) -> h2 (7+1) -> output (n*k)
-         parameter (nh1=12, nh2=7, iter=100, c1=2, c2=2)
-         nparticle = m*(nh1+1) + (nh1+1)*(nh2+1) + (nh2+1)*k
-         double precision particle(nparticle)
-         dim(m,      nh1+1)    :: w1, pbest1, gbest1
-         dim(nh1+1,  nh2+1)    :: w2, pbest2, gbest2
-         dim(nh2+1, k)         :: w3, pbest3, gbest3
-         dim(n, k)             :: output, yhat
-
-         call pso(particle, fit)
-       end subroutine nn
-C compute the network
-       subroutine fit(x, particle, y, yhat)
-         w1 = reshape(particle(1:m*(nh1+1)), (/m, nh1+1/))
-         w2 = reshape(particle((1+m*(nh1+1)):(m*(nh1+1)+(nh1+1)*(nh2+1))), (/nh+1, nh2+1/))
-         w3 = reshape(particle(:), (/nh2+1, k/))
-         yhat = x*w1*w2*w3
-         mean((y-yhat)**2)
-       end subroutine fit
+C       4 - 3 - 1
+C   
+         parameter (ni=4, nh=3, no = 1)
+         double precision w1(4, 3), w2(3, 1), b1(1, 3), b2
+C load in x iris 
+C load in y iris species 
+         x = ...
+         y = ... 
+         p = tanh(matmul(x, w1) + transpose(spread(b1, 3, 4)))
+         p = tanh(matmul(p, w2) + b2)
+C mse loss
+         nn = sum((y-p)**2))/nobs
+         
+       end function nn
